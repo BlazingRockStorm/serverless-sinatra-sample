bde3cf791dc3e89dec04e6a84c8aaf1feb279e8f
Hoang Duc Quan <rocker.gryphon@gmail.com>
2024-03-16 20:34:05 +0900

    Code coverage (#4)
    * edit gitignore
    
    * install code coverage
    
    * gen Github action
    
    * update
    
    * upload download report
    
    * refactoring

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,10 +2,18 @@
 
 require_relative '../app/server'
 require 'rack/test'
+require 'simplecov'
+SimpleCov.start
 
 set :environment, :test
 
 RSpec.configure do |config|
+end
+
+if ENV['RAILS_ENV'] == 'test'
+  require 'simplecov'
+  SimpleCov.start 'rails'
+  puts 'required simplecov'
 end
 
 def app
